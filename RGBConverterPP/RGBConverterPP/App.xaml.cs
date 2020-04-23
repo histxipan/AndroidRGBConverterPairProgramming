d4ea881c339eda501b0ebb07c1b33fe03034aa52
xipan <Xi Pan>
2020-04-23 23:30:15 +0200

    添加项目文件。

---
+++ RGBConverterPP/RGBConverterPP/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RGBConverterPP.Views;
+
+namespace RGBConverterPP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HelloWorld();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RGBConverterPP/RGBConverterPP/Views/HelloWorld.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RGBConverterPP.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelloWorld : ContentPage
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+    }
+}
